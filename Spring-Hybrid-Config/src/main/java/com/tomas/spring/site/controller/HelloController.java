3dffaa16358705a04105ff9f34dec2318e2c71e5
tomas-93 <tomasyussef@gmail.com>
2016-07-06 12:37:57 -0500

    Este commit -> Se termina la pequeña practica MVC -> Se crea la practica Spring form

--- Spring-Hybrid-Config/src/main/java/com/tomas/spring/site/controller/HelloController.java
+++ Spring-Hybrid-Config/src/main/java/com/tomas/spring/site/controller/HelloController.java
@@ -1,9 +1,8 @@
-package com.tomas.spring.controller;
+package com.tomas.spring.site.controller;
 
 import com.tomas.spring.model.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
